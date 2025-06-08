44c18da124a36dbd78c3b333aadffca256daf361
AruukeNurpaiaskyzy <aruukelisa@gmail.com>
2025-06-08 13:24:22 +0600

    added  code

--- firebase.py
+++ firebase.py
@@ -1,1 +1,10 @@
-
+import pyrebase
+config = {
+  apiKey: "AIzaSyBvkyTGDK1H92r7uq3gr7hPJ4LkaUPJB7c",
+  authDomain: "connectingfirebasedbtopy-52e63.firebaseapp.com",
+  projectId: "connectingfirebasedbtopy-52e63",
+  storageBucket: "connectingfirebasedbtopy-52e63.firebasestorage.app",
+  messagingSenderId: "219724928193",
+  appId: "1:219724928193:web:60daffa663adfcd3b23040",
+  measurementId: "G-BZ1E9EHRNJ"
+}
